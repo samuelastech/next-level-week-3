e5ff4a74c272c7849455cca1437c33d1535aff0a
SAMUEL <samuel.as729@gmail.com>
2021-03-09 23:28:27 -0300

    Adding mobile connection to Node backend

--- backend/src/server.ts
+++ backend/src/server.ts
@@ -15,12 +15,15 @@ const sslServer = https.createServer({
     cert: fs.readFileSync(path.join(__dirname, '..', 'cert', 'cert.pem')),
 }, app);
 
-app.use(cors());
+app.use(cors({
+	origin: true,
+	credentials: true,
+}));
 app.use(express.json());
 app.use(routes);
 app.use('/uploads', express.static(path.join(__dirname, '..', 'uploads')));
 app.use(errorHandler);
 
 sslServer.listen(3333, ()=>{
-    console.log('Secure Server Teste');
+    console.log('Server ready to be used');
 });
